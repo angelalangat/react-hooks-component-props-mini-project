412ef4a314b0fd5fd845a01448e5896c05ece6b1
angie <angelaclangat@gmai.com>
2024-08-01 11:15:56 +0300

    final commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function About({image,about}){
-    const defaultImage = 'https://via.placeholder.com/215'
+function About({image='https://via.placeholder.com/215',about}){
+
     return(
         <aside>
-            <img src={image || defaultImage} alt="bloglogo"/>
+            <img src={image} alt="blog logo"/>
             <p>{about}</p>
         </aside>
     )
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function Article({title, date='January 1, 1970', preview}){
+function Article({title, date='January 1, 1970', preview, minutes}){
+    let emoji = '';
+
+    if (minutes < 30) {
+        const cups = Math.ceil(minutes / 5);
+        emoji = '☕️'.repeat(cups);
+    } else {
+        const boxes = Math.ceil(minutes / 10);
+        emoji = '🍱'.repeat(boxes);
+    }
+
     return(
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} 
+                . 
+                {emoji}
+                {minutes} mins read
+            </small>
             <p>{preview}</p>
         </article>
     )
